814212c765c289d2252eaceb0e6fe99eaf88dda0
DESKTOP-OBN500O\zhang <zhang13652103006@163.com>
2018-08-02 19:54:12 +0800

    idpool fro tableid

---
+++ pools/id_pool.go
@@ -0,0 +1,73 @@
+package pools
+
+import (
+	"fmt"
+	"sync"
+)
+
+type IdPool struct {
+	sync.RWMutex
+	used      map[int64]bool
+	startId   int64
+	maxUsedId int64
+}
+
+func NewIdPool(startId int64) *IdPool {
+
+	return &IdPool{
+		used:      make(map[int64]bool, 0),
+		startId:   startId,
+		maxUsedId: startId,
+	}
+}
+
+func (pool *IdPool) Get() int64 {
+
+	pool.Lock()
+	defer pool.Unlock()
+
+	for id := range pool.used {
+
+		delete(pool.used, id)
+
+		return id
+	}
+
+	pool.maxUsedId = pool.maxUsedId + 1
+
+	return pool.maxUsedId
+}
+
+func (pool *IdPool) Put(id int64) {
+
+	pool.Lock()
+	defer pool.Unlock()
+
+	if id <= pool.startId || id > pool.maxUsedId {
+
+		panic(fmt.Errorf("IDPool.Put(%v): invalid value, must be in the range [%v,%v]", id, pool.startId, pool.maxUsedId))
+	}
+
+	if pool.used[id] {
+
+		panic(fmt.Errorf("IDPool.Put(%v): can't put value that was already recycled", id))
+	}
+
+	pool.used[id] = true
+}
+
+func (pool *IdPool) MaxUsedCount() int64 {
+
+	pool.RLock()
+	defer pool.RUnlock()
+
+	return pool.maxUsedId - pool.startId
+}
+
+func (pool *IdPool) CurrUsedCount() int64 {
+
+	pool.Lock()
+	defer pool.Unlock()
+
+	return pool.maxUsedId - pool.startId - int64(len(pool.used))
+}
---
+++ pools/id_pool_test.go
@@ -0,0 +1,146 @@
+package pools
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func (pool *IdPool) want(want *IdPool, t *testing.T) {
+
+	if pool.maxUsedId != want.maxUsedId {
+
+		t.Errorf("pool.maxUsed = %#v, want %#v", pool.maxUsedId, want.maxUsedId)
+	}
+
+	if !reflect.DeepEqual(pool.used, want.used) {
+
+		t.Errorf("pool.used = %#v, want %#v", pool.used, want.used)
+	}
+}
+
+func TestIDPoolFirstGet(t *testing.T) {
+
+	pool := NewIdPool(0)
+
+	if got := pool.Get(); got != 1 {
+
+		t.Errorf("pool.Get() = %v, want 1", got)
+	}
+
+	pool.want(&IdPool{used: map[int64]bool{}, maxUsedId: 1}, t)
+}
+
+func TestIDPoolSecondGet(t *testing.T) {
+
+	pool := NewIdPool(100)
+	pool.Get()
+
+	if got := pool.Get(); got != 2 {
+
+		t.Errorf("pool.Get() = %v, want 2", got)
+	}
+
+	pool.want(&IdPool{used: map[int64]bool{}, maxUsedId: 1, startId: 100}, t)
+}
+
+func TestIDPoolPutToUsedSet(t *testing.T) {
+
+	pool := NewIdPool(100)
+	id1 := pool.Get()
+	pool.Get()
+	pool.Put(id1)
+
+	pool.want(&IdPool{used: map[int64]bool{101: true}, maxUsedId: 102, startId: 100}, t)
+}
+
+func TestIDPoolPutMaxUsed(t *testing.T) {
+	pool := NewIdPool(0)
+	id1 := pool.Get()
+	pool.Put(id1)
+
+	pool.want(&IdPool{used: map[int64]bool{1: true}, maxUsedId: 1}, t)
+}
+
+func TestIDPoolMaxUsedCount(t *testing.T) {
+	pool := NewIdPool(0)
+	pool.Get()
+	id2 := pool.Get()
+	pool.Put(id2)
+	pool.Get()
+	id3 := pool.Get()
+	pool.Put(id3)
+
+	if hisMax := pool.MaxUsedCount(); hisMax != 3 {
+
+		t.Errorf("pool.MaxUsedCount() = %v, want 3", hisMax)
+	}
+
+	if curr := pool.CurrUsedCount(); curr != 2 {
+
+		t.Errorf("pool.CurrUsedCount() = %v, want 2", curr)
+	}
+
+	pool.want(&IdPool{used: map[int64]bool{3: true}, maxUsedId: 3}, t)
+}
+
+func TestIDPoolGetFromUsedSet(t *testing.T) {
+
+	pool := NewIdPool(0)
+	id1 := pool.Get()
+	pool.Get()
+	pool.Put(id1)
+
+	if got := pool.Get(); got != 1 {
+
+		t.Errorf("pool.Get() = %v, want 1", got)
+	}
+
+	pool.want(&IdPool{used: map[int64]bool{}, maxUsedId: 2}, t)
+}
+
+func wantError(want string, t *testing.T) {
+
+	rec := recover()
+	if rec == nil {
+
+		t.Errorf("expected panic, but there wasn't one")
+	}
+
+	err, ok := rec.(error)
+	if !ok || !strings.Contains(err.Error(), want) {
+
+		t.Errorf("wrong error, got '%v', want '%v'", err, want)
+	}
+}
+
+func TestIDPoolPut0(t *testing.T) {
+
+	pool := NewIdPool(0)
+
+	defer wantError("invalid value", t)
+
+	pool.Put(0)
+}
+
+func TestIDPoolPutInvalid(t *testing.T) {
+
+	pool := NewIdPool(0)
+	pool.Get()
+
+	defer wantError("invalid value", t)
+
+	pool.Put(5)
+}
+
+func TestIDPoolPutDuplicate(t *testing.T) {
+
+	pool := NewIdPool(0)
+	pool.Get()
+	pool.Get()
+	pool.Put(1)
+
+	defer wantError("already recycled", t)
+
+	pool.Put(1)
+}
