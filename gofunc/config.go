ca5fddfaef40e79980d0bfe806194dd3d43dd55d
DESKTOP-OBN500O\zhang <zhang13652103006@163.com>
2018-08-11 17:35:09 +0800

    add gameDataConf

--- gofunc/config.go
+++ gofunc/config.go
@@ -5,34 +5,50 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 )
 
 func init() {
 
-	AppPath, _ = filepath.Abs(filepath.Dir(os.Args[0]))
-	AppPath = AppPath + string(os.PathSeparator)
-	AppPath = filepath.Join(AppPath + "..")
+	appPath, _ = filepath.Abs(filepath.Dir(os.Args[0]))
+	appPath = appPath + string(os.PathSeparator)
+	appPath = filepath.Join(appPath + "..")
+
+	dataConf = &gameDataConf{data: make(map[string]interface{})}
 }
 
-var AppPath string
+var appPath string
+var dataConf *gameDataConf
 
 const (
 	CONFIGS = "configs"
 	DATAS   = "datas"
 )
 
+// gameDataConf 游戏策划配置
+type gameDataConf struct {
+	mux  sync.RWMutex
+	data map[string]interface{}
+}
+
 //LoadJsonConf 加载json静态配置数据
 //model：configs or data
 //name: 文件名
 func LoadJsonConf(model, name string, v interface{}) {
 
-	fileName := filepath.Join(AppPath, model, name+".json")
+	fileName := filepath.Join(appPath, model, name+".json")
 
 	err := loadJsonFile(fileName, v)
 	if err != nil {
 
 		panic(err)
 	}
+}
+
+//GetAppPath 获取应用路径
+func GetAppPath() string {
+
+	return appPath
 }
 
 func loadJsonFile(file string, v interface{}) error {
@@ -46,4 +62,22 @@ func loadJsonFile(file string, v interface{}) error {
 	err = json.Unmarshal(buf, v)
 
 	return err
+}
+
+// GetdataConf 数据
+func GetDataConf(key string) interface{} {
+
+	dataConf.mux.RLock()
+	defer dataConf.mux.RUnlock()
+
+	return dataConf.data[key]
+}
+
+// Set 数据
+func SetDataConf(key string, v interface{}) {
+
+	dataConf.mux.Lock()
+	defer dataConf.mux.Unlock()
+
+	dataConf.data[key] = v
 }
