3cb98aa2de6c0464890d913e720c42dfd8b44c25
laonsx <zhang13652103006@163.com>
2018-08-23 10:59:53 +0800

    Update codec.go

--- codec/codec.go
+++ codec/codec.go
@@ -6,14 +6,18 @@ import (
 	"github.com/ugorji/go/codec"
 )
 
+var (
+	msgpackHandle codec.MsgpackHandle
+)
+
 func MsgPack(v interface{}) ([]byte, error) {
 	byteBuf := new(bytes.Buffer)
-	enc := codec.NewEncoder(byteBuf, &codec.MsgpackHandle)
+	enc := codec.NewEncoder(byteBuf, &msgpackHandle)
 	err := enc.Encode(v)
 	return byteBuf.Bytes(), err
 }
 
 func UnMsgPack(data []byte, v interface{}) error {
-	dec := codec.NewDecoder(bytes.NewReader(data), &codec.MsgpackHandle)
+	dec := codec.NewDecoder(bytes.NewReader(data), &msgpackHandle)
 	return dec.Decode(v)
 }
